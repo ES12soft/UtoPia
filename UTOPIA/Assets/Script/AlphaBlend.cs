4ebb5e130032b9ac4c783a6461b04ebf699e78de
LeeHyeA <951220_@naver.com>
2017-07-04 17:00:41 +0900

    합성(해야함), 아이템 사라지기 효과 (하다말음)

---
+++ UTOPIA/Assets/Script/AlphaBlend.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// 합성시스템
+
+
+public class AlphaBlend : MonoBehaviour {
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(Input.mousePosition);
+    }
+
+    public void AlphaBlendClick()
+    {
+        Debug.Log("클릭");
+        Image image = this.gameObject.GetComponent<Image>();
+        int speed = 2;
+        var color = image.color;
+        if (color.a > 0)
+            color.a -= Time.deltaTime * speed;
+
+        else
+            Destroy(this.gameObject);
+    }
+}
+
