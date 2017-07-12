95bc287952bbc91905d3c9bebe288990a10ae2a0
SungBae <binky7@naver.com>
2017-07-12 20:02:54 +0900

    -

--- Utopia (develop)-수정/Assets/Script/SB/Data.cs
+++ Utopia (develop)-수정/Assets/Script/SB/Data.cs
@@ -2,11 +2,10 @@
 //using System.Collections.Generic;
 //using UnityEngine;
 
-<<<<<<< HEAD
 ////public class Data : MonoBehaviour {
-=======
+
 //public class Data : MonoBehaviour {
->>>>>>> d31d4bb0aa3561a14badf40df1283de83572df03
+
 
 //	// Use this for initialization
 //	void Start () {
