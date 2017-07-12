fff970774f3d9676e463745deab1b1d46368be61
김준연 <mynamejohn@naver.com>
2017-07-12 21:34:10 +0900

    07-12

--- Utopia (develop)-수정/Assets/Script/JY/ControlDialogue.cs
+++ Utopia (develop)-수정/Assets/Script/JY/ControlDialogue.cs
@@ -64,6 +64,7 @@ public class ControlDialogue : MonoBehaviour
                 isActive = false;
                 Debug.Log("끝");
                 EM.Event_Number++;
+                EM.Doing_Event = false;
                 return;
             }
             else
@@ -76,7 +77,7 @@ public class ControlDialogue : MonoBehaviour
 
                 Stand_Image.sprite = Resources.Load<Sprite>(imagestring);
                 Stand_Image.color = new Color(Stand_Image.color.r, Stand_Image.color.g, Stand_Image.color.b, 255);
-                Panel_Image.color = new Color(Stand_Image.color.r, Stand_Image.color.g, Stand_Image.color.b, 255);
+                Panel_Image.color = new Color(Panel_Image.color.r, Panel_Image.color.g, Panel_Image.color.b,Panel_Image.color.a);
 
             }
 
